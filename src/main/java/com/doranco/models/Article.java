4b23f9d44c8e089732df6c13f134325b785a1b86
Chrystal Bastid <chrystal.bastid@gmail.com>
2023-06-19 15:00:28 +0200

    update classe data base

--- src/main/java/com/doranco/models/Article.java
+++ src/main/java/com/doranco/models/Article.java
@@ -7,25 +7,29 @@ public class Article {
     private double prix;
     private boolean vendu;
     private String photo;
-    private double tauxRemise;
+    private double remise; // en pourcentage (%)
     private int stock;
+    private String description;
+    private String commentaire;
     
     //Constructeur
-
     public Article() {
     }
 
-    public Article(int id, String nom, double prix, boolean vendu, String photo, double tauxRemise, int stock) {
+    public Article(int id, String nom, double prix, boolean vendu, String photo, 
+            double remise, int stock, String description, String commentaire) {
         this.id = id;
         this.nom = nom;
         this.prix = prix;
         this.vendu = vendu;
         this.photo = photo;
-        this.tauxRemise = tauxRemise;
+        this.remise = remise;
         this.stock = stock;
+        this.description = description;
+        this.commentaire = commentaire;
     }
     
-    //Getter & Setter
+    // Getters
     public int getId() {
         return id;
     }
@@ -46,14 +50,23 @@ public class Article {
         return photo;
     }
 
-    public double getTauxRemise() {
-        return tauxRemise;
+    public double getRemise() {
+        return remise;
     }
 
     public int getStock() {
         return stock;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    // Setters
     public void setId(int id) {
         this.id = id;
     }
@@ -74,13 +87,25 @@ public class Article {
         this.photo = photo;
     }
 
-    public void setTauxRemise(double tauxRemise) {
-        this.tauxRemise = tauxRemise;
+    public void setRemise(double remise) {
+        this.remise = remise;
     }
 
     public void setStock(int stock) {
         this.stock = stock;
     }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
     
+    // Méthode pour obtenir la remise en pourcentage
+    public double getRemiseEnPourcentage() {
+        return remise * 100; // Multiplier par 100 pour obtenir le pourcentage
+    }
     
 }
--- src/main/java/com/doranco/models/Utilisateur.java
+++ src/main/java/com/doranco/models/Utilisateur.java
@@ -1,4 +1,163 @@
-package models;
+package com.doranco.models;
+
+import java.util.Date;
+import java.util.List;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class Utilisateur {
+    //Variables
+    private int id;
+    private String nom;
+    private String prenom;
+    private Date dateNaissance;
+    private boolean actif;
+    private String email;
+    private String motDePasse; /*mdp crypter*/
+    private String numeroTelephone;
+    private Adresse adresse;
+    private List<Commande> commandes;
+    private CartePaiement cartePaiement;
+    private Commentaire commentaire;
+    private List<ArticlePanier> panier;
+    
+    //Constructeurs
+    public Utilisateur(int id, String nom, String prenom, Date dateNaissance, boolean actif, String email, String motDePasse, String numeroTelephone, Adresse adresse, List<Commande> commandes, CartePaiement cartePaiement, Commentaire commentaire, List<ArticlePanier> panier) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.actif = actif;
+        this.email = email;
+        this.motDePasse = motDePasse;
+        this.numeroTelephone = numeroTelephone;
+        this.adresse = adresse;
+        this.commandes = commandes;
+        this.cartePaiement = cartePaiement;
+        this.commentaire = commentaire;
+        this.panier = panier;
+    }
+
+    public Utilisateur(){
+    }
+    
+    //Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public boolean isActif() {
+        return actif;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public String getNumeroTelephone() {
+        return numeroTelephone;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public List<Commande> getCommandes() {
+        return commandes;
+    }
+
+    public CartePaiement getCartePaiement() {
+        return cartePaiement;
+    }
+    
+    public Commentaire getCommentaire(){
+        return commentaire;
+    }
+
+    public List<ArticlePanier> getPanier() {
+        return panier;
+    }
+
+    //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public void setActif(boolean actif) {
+        this.actif = actif;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String motDePasseCrypte = encoder.encode(motDePasse);
+        this.motDePasse = motDePasseCrypte;
+    }
+
+    public void setNumeroTelephone(String numeroTelephone) {
+        this.numeroTelephone = numeroTelephone;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+
+    public void setCommandes(List<Commande> commandes) {
+        this.commandes = commandes;
+    }
+
+    public void setCartePaiement(CartePaiement cartePaiement) {
+        this.cartePaiement = cartePaiement;
+    }
+    
+    public void setCommentaire(Commentaire commentaire){
+        this.commentaire = commentaire;
+    }
+
+    public void setPanier(List<ArticlePanier> panier) {
+        this.panier = panier;
+    }
+    
+    // Méthode pour vérifier le mot de passe
+    public boolean verifierMotDePasse(String motDePasse) {
+        // Création d'une instance de BCryptPasswordEncoder, qui est un 
+        // algorithme de hachage fort utilisé pour le hachage sécurisé des mdp
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        
+        // Appel de la méthode matches de l'objet encoder pour comparer le 
+        // mdp fourni en paramètre avec le mdp haché stocké dans l'objet Utilisateur
+        // La méthode matches renvoie true si les deux mots de passe correspondent, et false sinon
+        return encoder.matches(motDePasse, this.motDePasse);
+    }
+    
 }
