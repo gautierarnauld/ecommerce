663a1dfea73c782c2ecdc303455b3bc08a304fbf
Chrystal Bastid <chrystal.bastid@gmail.com>
2023-06-19 12:15:03 +0200

    home page created

--- src/main/java/com/doranco/controller/AccueilController.java
+++ src/main/java/com/doranco/controller/AccueilController.java
@@ -1,13 +1,31 @@
 package com.doranco.controller;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AccueilController {
     @GetMapping("/")
-    public String accueil(){
-        return "accueil"; // Retourne le nom de la vue à afficher (accueil.xhtml)
+    public String accueil() {
+        org.springframework.security.core.Authentication authentication = 
+                SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .anyMatch(role -> role.equals("ROLE_MAGASINIER"))) {
+            return "redirect:/gestion-articles";
+        }
+
+        return "redirect:/accueil";
+    }
+
+    @GetMapping("/gestion-articles")
+    public String gestionArticles() {
+        // Logique de gestion des articles
+        // Retourne le nom de la vue à afficher (gestion-articles.xhtml)
+        return "gestion-articles";
     }
 
 }
