63e6fe78be66b6cf0b467670c8eab279cc68cd23
Chrystal Bastid <chrystal.bastid@gmail.com>
2023-06-21 16:33:52 +0200

    created method ajouterArticlePanier

--- src/main/java/com/doranco/services/ArticlePanierService.java
+++ src/main/java/com/doranco/services/ArticlePanierService.java
@@ -12,6 +12,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArticlePanierService {
+    
+    public void ajouterArticlePanier(ArticlePanier articlePanier) {
+        String query = "INSERT INTO ArticlePanier (id, articleId, quantite, utilisateurId) " +
+                       "VALUES (?, ?, ?, ?)";
+
+        try (Connection connection = DatabaseConnector.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, articlePanier.getId());
+            statement.setInt(2, articlePanier.getArticle().getId());
+            statement.setInt(3, articlePanier.getQuantite());
+            statement.setInt(4, articlePanier.getUtilisateur().getId());
+
+            statement.executeUpdate();
+            System.out.println("L'article a été ajouté au panier.");
+        } catch (SQLException e) {
+            System.err.println("Erreur lors de l'ajout de l'article au panier : " + e.getMessage());
+        }
+    }
         
     public List<ArticlePanier> getPanierByUtilisateurId(int utilisateurId) {
         List<ArticlePanier> panier = new ArrayList<>();
