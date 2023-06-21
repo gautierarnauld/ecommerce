b1fdd884a494045b8898f60d35423ef06d596c47
Chrystal Bastid <chrystal.bastid@gmail.com>
2023-06-21 12:38:46 +0200

    organization of the code (class and method separation)

--- src/main/java/com/doranco/models/Utilisateur.java
+++ src/main/java/com/doranco/models/Utilisateur.java
@@ -1,10 +1,5 @@
 package com.doranco.models;
 
-import com.doranco.utils.DatabaseConnector;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-
-import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -151,44 +146,5 @@ public class Utilisateur {
 
     public void setPanier(List<ArticlePanier> panier) {
         this.panier = panier;
-    }
-    
-    // Méthode pour vérifier le mot de passe
-    public boolean verifierMotDePasse(String motDePasse) {
-        // Création d'une instance de BCryptPasswordEncoder, qui est un 
-        // algorithme de hachage fort utilisé pour le hachage sécurisé des mdp
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        
-        // Appel de la méthode matches de l'objet encoder pour comparer le 
-        // mdp fourni en paramètre avec le mdp haché stocké dans l'objet Utilisateur
-        // La méthode matches renvoie true si les deux mots de passe correspondent, et false sinon
-        return encoder.matches(motDePasse, this.motDePasse);
-    }
-    
-    /*Méthode pour ajouter un user dans la base de données*/
-    public void ajouterUtilisateur(Utilisateur utilisateur) {
-        String query = "INSERT INTO Utilisateur (id, nom, prenom, dateNaissance, actif, email, motDePasse, numeroTelephone, adresseId) " +
-                       "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-        try (Connection connection = DatabaseConnector.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setInt(1, utilisateur.getId());
-            statement.setString(2, utilisateur.getNom());
-            statement.setString(3, utilisateur.getPrenom());
-            statement.setDate(4, new java.sql.Date(utilisateur.getDateNaissance().getTime()));
-            statement.setBoolean(5, utilisateur.isActif());
-            statement.setString(6, utilisateur.getEmail());
-            statement.setString(7, utilisateur.getMotDePasse());
-            statement.setString(8, utilisateur.getNumeroTelephone());
-            statement.setInt(9, utilisateur.getAdresse().getId());
-
-            statement.executeUpdate();
-            System.out.println("L'utilisateur a été ajouté à la base de données.");
-        } catch (SQLException e) {
-            System.err.println("Erreur lors de l'ajout de l'utilisateur : " + e.getMessage());
-        }
-    }
-
-
-    
+    }   
 }
