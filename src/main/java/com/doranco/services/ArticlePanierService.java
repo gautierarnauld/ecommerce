d7a3493d722a500201e6d9427adeb1f5fa4c2eea
Chrystal Bastid <chrystal.bastid@gmail.com>
2023-06-21 12:44:02 +0200

    created method getPanierByUtilisateurId

---
+++ src/main/java/com/doranco/services/ArticlePanierService.java
@@ -0,0 +1,45 @@
+package com.doranco.services;
+
+import com.doranco.models.Article;
+import com.doranco.models.ArticlePanier;
+import com.doranco.models.Utilisateur;
+import com.doranco.utils.DatabaseConnector;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticlePanierService {
+        
+    public List<ArticlePanier> getPanierByUtilisateurId(int utilisateurId) {
+        List<ArticlePanier> panier = new ArrayList<>();
+        String query = "SELECT * FROM ArticlePanier WHERE utilisateurId = ?";
+
+        try (Connection connection = DatabaseConnector.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, utilisateurId);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    int articleId = resultSet.getInt("articleId");
+                    int quantite = resultSet.getInt("quantite");
+                  
+                    ArticleService articleService = new ArticleService();
+                    Article article = articleService.getArticleById(articleId);
+                    
+                    UtilisateurService utilisateurService = new UtilisateurService();
+                    Utilisateur utilisateur = utilisateurService.getUtilisateurById(utilisateurId);
+
+                    ArticlePanier articlePanier = new ArticlePanier(id, article, quantite, utilisateur);
+                    panier.add(articlePanier);
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Erreur lors de la récupération du panier : " + e.getMessage());
+        }
+
+        return panier;
+    }
+}
