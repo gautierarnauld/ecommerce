f1128d725ad22cad785e91076d1f4c8c3391ea5b
Chrystal Bastid <chrystal.bastid@gmail.com>
2023-06-21 12:34:01 +0200

    Modification of the hand with addition of category and payment card

--- src/main/java/com/doranco/ecommerce/ECommerce.java
+++ src/main/java/com/doranco/ecommerce/ECommerce.java
@@ -4,37 +4,55 @@ import com.doranco.models.Adresse;
 import com.doranco.models.Article;
 import com.doranco.models.Utilisateur;
 import com.doranco.models.Commentaire;
-import java.util.Date;
+import com.doranco.models.CartePaiement;
+import com.doranco.models.Categorie;
+import com.doranco.models.Commande;
+import com.doranco.services.AdresseService;
+import com.doranco.services.ArticleService;
+import com.doranco.services.CartePaiementService;
+import com.doranco.services.CategorieService;
+import com.doranco.services.CommandeService;
+import com.doranco.services.CommentaireService;
+import com.doranco.services.UtilisateurService;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ECommerce {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //- - - - - - - - Création d'un nouvel article - - - - - - - - 
-        /*Article nouvelArticle = new Article();
-        nouvelArticle.setId(3);
-        nouvelArticle.setNom("Nounours viking");
-        nouvelArticle.setPrix(19.90);
-        nouvelArticle.setVendu(false);
-        nouvelArticle.setPhoto("https://boutique.puydufou.com/media/catalog/product/p/e/peluche-ours-viking-2_1.png");
-        nouvelArticle.setRemise(0.2); // 20% de remise
-        nouvelArticle.setStock(2);
-        nouvelArticle.setDescription("Ce petit ours Viking vous protègera contre les attaques de barbares !");
-        nouvelArticle.setCommentaire("J'adore");
-
+        /*ArticleService articleService = new ArticleService();
+        
+        Article article = new Article();
+        article.setId(9);
+        article.setNom("Poulet roti");
+        article.setPrix(9.99);
+        article.setVendu(true);
+        article.setPhoto("https://www.undejeunerdesoleil.com/wp-content/uploads/2023/01/Reussir_poulet_roti_secret_recette.jpg");
+        article.setRemise(0.2); // 20% de remise
+        article.setStock(10);
+        article.setDescription("au barbecue");
+        article.setCommentaire("");
+        
         // Ajout de l'article à la base de données
-        nouvelArticle.ajouterArticle();*/
+        articleService.ajouterArticle(article);*/
                 
         //- - - - - - - -  Création d'un objet adresse - - - - - - - - 
-        /*Adresse adresse = new Adresse();
+        /*AdresseService adresseService = new AdresseService();
+        
+        Adresse adresse = new Adresse();
         adresse.setId(10);
         adresse.setNumero("1213");
         adresse.setRue("Rue de la Paix");
         adresse.setVille("Toulouse");
         adresse.setCodePostal("31000");
 
-        adresse.ajouterAdresse();*/
+        adresseService.ajouterAdresse(adresse);*/
         
         //- - - - - - - - Création d'un objet utilisateur - - - - - - - - 
-        /*Adresse adresse1 = new Adresse();
+        /*UtilisateurService utilisateurService = new UtilisateurService();
+        
+        Adresse adresse1 = new Adresse();
         adresse1.setId(8);        
 
         Utilisateur utilisateur1 = new Utilisateur();
@@ -48,9 +66,11 @@ public class ECommerce {
         utilisateur1.setNumeroTelephone("9876543210");     
         utilisateur1.setAdresse(adresse1);
 
-        utilisateur1.ajouterUtilisateur(utilisateur1);*/
+        utilisateurService.ajouterUtilisateur(utilisateur1);*/
         
         //- - - - - - - - Création d'un nouveau commentaire - - - - - - - - 
+        /*CommentaireService commentaireService = new CommentaireService();
+        
         Commentaire commentaire = new Commentaire();
         commentaire.setId(3); 
         commentaire.setTexte("Vraiment pas ouf !!!!");
@@ -59,8 +79,69 @@ public class ECommerce {
         int articleId = 3;
         int utilisateurId = 5; 
 
-        commentaire.ajouterCommentaire(commentaire, articleId, utilisateurId);
+        commentaireService.ajouterCommentaire(commentaire, articleId, utilisateurId);*/
 
+        //- - - - - - - - Création d'une carte de paiement - - - - - - - - 
+        /*CartePaiementService cartePaiementService = new CartePaiementService();
+        
+        CartePaiement cartePaiement = new CartePaiement();
+        cartePaiement.setId(6);
+        cartePaiement.setNomProprietaire("Martine");
+        cartePaiement.setPrenomProprietaire("Anne");
+        cartePaiement.setNumero("9119480800223456");
+        cartePaiement.setDateFinValidite(new java.sql.Date(2024, 2, 1));
+        cartePaiement.setCryptogramme("349");
+        
+        int userId = 6;
+        
+        cartePaiementService.ajouterCartePaiement(cartePaiement, userId);*/
 
+        //- - - - - - - - Création d'une commande - - - - - - - - 
+        /*CommandeService commandeService = new CommandeService();
+        
+        Commande nouvelleCommande = new Commande();
+        nouvelleCommande.setId(3);
+        nouvelleCommande.setNumero("CMD003");
+        nouvelleCommande.setDateCreation("21.06.2023");
+        nouvelleCommande.setDateLivraison("06.07.2023");
+        nouvelleCommande.setTotaleRemise(12.0);
+        nouvelleCommande.setFraisExpedition(3.4);
+        nouvelleCommande.setTotalGeneral(19.0);
+        
+        Adresse adresseFacturation = new Adresse();
+        adresseFacturation.setId(6);
+        
+        Adresse adresseLivraison = new Adresse();
+        adresseLivraison.setId(6);
+        
+        CartePaiement cartePaiementCommande = new CartePaiement();
+        cartePaiementCommande.setId(6);
+        
+        Utilisateur utilisateur = new Utilisateur();
+        utilisateur.setId(6);
+        
+        nouvelleCommande.setAdresseFacturation(adresseFacturation);
+        nouvelleCommande.setAdresseLivraison(adresseLivraison);
+        nouvelleCommande.setCartePaiementDefaut(cartePaiementCommande);
+        nouvelleCommande.setUtilisateur(utilisateur);
+        
+        commandeService.ajouterCommande(nouvelleCommande);*/
+               
+        //- - - - - - - - Création d'une categorie - - - - - - - - 
+        /*Categorie nouvelleCategorie = new Categorie();
+        nouvelleCategorie.setId(3);
+        nouvelleCategorie.setNom("Nouriture");
+        nouvelleCategorie.setRemise(0.0);
+        nouvelleCategorie.setRemiseCumulable(true);
+        nouvelleCategorie.setPhoto("");
+
+        // Ajout des IDs des articles associés
+        List<Integer> articleIds = Arrays.asList(9, 7, 8);
+
+        nouvelleCategorie.setArticleIds(articleIds);
+
+        // Appel du service pour ajouter la catégorie
+        CategorieService categorieService = new CategorieService();
+        categorieService.ajouterCategorie(nouvelleCategorie);*/
     }
 }
