cc875e375b61508af81a80030c89c96cf3ab4609
Chrystal Bastid <chrystal.bastid@gmail.com>
2023-06-22 11:04:06 +0200

    comments tue web service commentaire

--- src/main/java/com/doranco/webservice/CommentaireWebService.java
+++ src/main/java/com/doranco/webservice/CommentaireWebService.java
@@ -20,14 +20,19 @@ public class CommentaireWebService {
         commentaireService = new CommentaireService();
     }
 
+    // Endpoint pour récupérer les commentaires d'un article spécifié par son ID
     @GetMapping("/article/{id}")
     public ResponseEntity<List<Commentaire>> getCommentairesByArticleId(@PathVariable("id") int articleId) {
+        // Appel à la méthode du service pour obtenir les commentaires par ID de l'article
         List<Commentaire> commentaires = commentaireService.getCommentairesByArticleId(articleId);
 
+        // Vérifier si aucun commentaire n'a été trouvé
         if (commentaires == null || commentaires.isEmpty()) {
+            // Retourner une réponse indiquant que les commentaires n'ont pas été trouvés (code 404)
             return ResponseEntity.notFound().build();
         }
 
+        // Retourner les commentaires trouvés dans une réponse avec un code 200 (OK)
         return ResponseEntity.ok(commentaires);
     }
 }
