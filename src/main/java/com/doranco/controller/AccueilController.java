30ddbe38dfc426ae45369465da7b8f7eefcea9ac
Chrystal Bastid <chrystal.bastid@gmail.com>
2023-06-23 10:35:00 +0200

    code formatting

--- src/main/java/com/doranco/controller/AccueilController.java
+++ src/main/java/com/doranco/controller/AccueilController.java
@@ -14,26 +14,26 @@ public class AccueilController {
     /*GetMapping signifie que la classe répond aux requêtes GET sur la racine du contexte de l'application*/
     @GetMapping("/")
     public String accueil() {
-    // Récupérer l'objet d'authentification de l'utilisateur actuel
-    org.springframework.security.core.Authentication authentication =
-    SecurityContextHolder.getContext().getAuthentication();
+        // Récupérer l'objet d'authentification de l'utilisateur actuel
+        org.springframework.security.core.Authentication authentication =
+        SecurityContextHolder.getContext().getAuthentication();
+
+        // Vérifier si l'utilisateur est authentifié et possède le rôle "ROLE_MAGASINIER"
+        if (authentication != null && authentication.getAuthorities().stream()
+            .map(GrantedAuthority::getAuthority)
+            .anyMatch(role -> role.equals("ROLE_MAGASINIER"))) {
+            // Rediriger l'utilisateur vers la page de gestion des articles
+            return "redirect:/gestion-articles";
+        }
     
-    // Vérifier si l'utilisateur est authentifié et possède le rôle "ROLE_MAGASINIER"
-    if (authentication != null && authentication.getAuthorities().stream()
-    .map(GrantedAuthority::getAuthority)
-    .anyMatch(role -> role.equals("ROLE_MAGASINIER"))) {
-    // Rediriger l'utilisateur vers la page de gestion des articles
-    return "redirect:/gestion-articles";
-    }
-    
-    // Rediriger l'utilisateur vers la page d'accueil par défaut
-    return "redirect:/accueil";
+        // Rediriger l'utilisateur vers la page d'accueil par défaut
+        return "redirect:/accueil";
     }
     
     @GetMapping("/gestion-articles")
     public String gestionArticles() {
-    // Logique de gestion des articles à implémenter ici
-    // Retourne le nom de la vue à afficher (dans ce cas, "gestion-articles.xhtml")
-    return "gestion-articles";
+        // Logique de gestion des articles à implémenter ici
+        // Retourne le nom de la vue à afficher (dans ce cas, "gestion-articles.xhtml")
+        return "gestion-articles";
     }
 }
