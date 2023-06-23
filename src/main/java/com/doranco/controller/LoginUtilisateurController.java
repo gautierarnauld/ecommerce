f5a6a44310fb1ac42da8624ae801260cd16ca7f1
Chrystal Bastid <chrystal.bastid@gmail.com>
2023-06-23 10:35:46 +0200

    comment out all code

--- src/main/java/com/doranco/controller/LoginUtilisateurController.java
+++ src/main/java/com/doranco/controller/LoginUtilisateurController.java
@@ -1,22 +1,22 @@
 package com.doranco.controller;
 
-import enumeration.ProfilUtilisateur;
+//import enumeration.ProfilUtilisateur;
 import org.springframework.stereotype.Controller;
 
 @Controller
 //@RequestMapping("/utilisateur")
 public class LoginUtilisateurController {
 
-    private ProfilUtilisateur getProfilUtilisateur(String username) {
+    /*private ProfilUtilisateur getProfilUtilisateur(String username) {
         /* Méthode pour obtenir le profil de l'utilisateur à partir de la base de données
         Elle devrait retourner le profil de l'utilisateur en fonction de son nom d'utilisateur
         ou renvoyer une valeur par défaut si le profil n'est pas trouvé*/
 
         // Retourne un profil en fonction du nom d'utilisateur
-        if (username.equals("magasinier")) {
+        /*if (username.equals("magasinier")) {
             return ProfilUtilisateur.MAGASINIER;
         } else {
             return ProfilUtilisateur.CLIENT;
         }
-    }
+    }*/
 }
